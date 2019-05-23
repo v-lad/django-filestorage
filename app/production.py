44b0f811a0765423b82ee01486e4cc2ed3dd866e
FMShelbyGT500 <eightynov@gmail.com>
2019-05-23 18:18:26 +0300

    Fix prod settings

--- app/production.py
+++ app/production.py
@@ -5,4 +5,4 @@ DEBUG = os.environ['DEBUG_ON']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['https://filestorage-evotask.herokuapp.com']
+ALLOWED_HOSTS = ['filestorage-evotask.herokuapp.com']
