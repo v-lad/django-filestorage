19f8df90c4be40787a1b98995f6d7a7d0927d84e
FMShelbyGT500 <eightynov@gmail.com>
2019-09-06 02:44:32 +0300

    Change MEDIA_ROOT

--- app/settings.py
+++ app/settings.py
@@ -96,8 +96,8 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
