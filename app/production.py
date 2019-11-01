34873f81f51d738e1e13d0b050d08a78fcb3b8e8
FMShelbyGT500 <eightynov@gmail.com>
2019-11-01 03:11:25 +0200

    Fix static storage

--- app/production.py
+++ app/production.py
@@ -1,7 +1,6 @@
 from .settings import *
 
-
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -18,21 +17,20 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
         },
     },
 }
 
-
 AWS_S3_SECURE_URLS = False
 AWS_QUERYSTRING_AUTH = False
-AWS_S3_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID'] 
+AWS_S3_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
 AWS_S3_SECRET_ACCESS_KEY = os.environ["AWS_S3_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
-AWS_S3_HOST = os.environ["AWS_S3_HOST"]  
+AWS_S3_HOST = os.environ["AWS_S3_HOST"]
 
 DEFAULT_FILE_STORAGE = "app.s3utils.MediaS3BotoStorage"
 
 import ssl
 if hasattr(ssl, '_create_unverified_context'):
-   ssl._create_default_https_context = ssl._create_unverified_context
+    ssl._create_default_https_context = ssl._create_unverified_context
